47e2c2f2ab104f915beb1104d5332c8f72d05a37
eduardog3000 <eduardog3000@gmail.com>
2019-04-10 12:37:57 -0400

    Added index page for GitHub Pages.
    Also added a missing semicolon.

--- lesson-08/01-selecting-nodes/script.js
+++ lesson-08/01-selecting-nodes/script.js
@@ -7,7 +7,7 @@ var currentPrevH2 = currentSection.previousElementSibling.querySelector('h2');
 // var sectionsWithH2 = document.querySelectorAll('section:has(h2)');
 // :has() is not yet supported :(
 var divWithH2Highlight = document.querySelector('h2.highlight').parentElement.parentElement;
-var sectionsWithH2 = [...document.querySelectorAll('section')].filter(e => e.querySelector('h2') != null)
+var sectionsWithH2 = [...document.querySelectorAll('section')].filter(e => e.querySelector('h2') != null);
 // Or Array.from() instead of spread, but spread looks nicer.
 // Or [...document.querySelectorAll('h2')].map(e => e.parentElement)
 //     but that would get parents of h2s that aren't sections if they existed.
